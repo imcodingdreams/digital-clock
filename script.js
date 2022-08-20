72d48833ef8c52154d0453bb0f2824a434b0d6e5
imcodingdreams <naykavallejo@yahoo.com>
2022-08-20 09:41:43 -0700

    Clock fixed

--- script.js
+++ script.js
@@ -1,22 +1,108 @@
 
 const currentDate = new Date();
-let cDay = currentDate.getDay();
-let cMonth = currentDate.getDate();
-let cYear = currentDate.getFullYear();
-
+// let cDay = currentDate.getDay();
+// let cMonth = currentDate.getDate();
+// let cYear = currentDate.getFullYear();
+// let cHour = currentDate.getHours();
+// let cMinute = currentDate.getMinutes();
+// let cSecond = currentDate.getSeconds();
 
 function displayTime() {
   const currentDate = new Date();
-  let cDay = currentDate.getDay();
-  let cMonth = currentDate.getDate();
-  let cYear = currentDate.getFullYear();
-  document.getElementById("currentTime").textContent = currentDate.toTimeString();
+  let cHour = currentDate.getHours();
+  let cMinute = currentDate.getMinutes();
+  let cSecond = currentDate.getSeconds();
+  let AmPm = cHour >= 12 ? "PM" : "AM"; // Check whether AM or PM
+  cHour = cHour % 12; // To make 12 hr format
+  cHour = cHour ? cHour : 12;  // To display "0" as "12"
+  cMinute = cMinute < 10 ? "0" + cMinute : cMinute;
+  cSecond = cSecond < 10 ? "0" + cSecond : cSecond;
+  document.getElementById("currentTime").innerText =
+    cHour + " : " + cMinute + " : " + cSecond + " " + AmPm;
 }
 
 setInterval(displayTime, 1000);
 
-// console.log(currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds());
 
+function displayDate() {
+  let cDay = currentDate.getDay();
+  let cMonth = currentDate.getMonth();
+  let cYear = currentDate.getFullYear();
+  // document.getElementById("currentDate").innerText = cDay + "," + cMonth + " " + cYear;
+  document.getElementById("currentDate").innerHTML = currentDate.toDateString();
+}
+  
+displayDate();
+
+
+// function displayTime() {
+//   const currentDate = new Date()
+//   let cHour = currentDate.getHours();
+//   let cMinute = currentDate.getMinutes();
+//   let cSecond = currentDate.getSeconds();
+//   let AmPm = cHour >= 12 ? "PM" : "AM";
+//   cMinute = addZero(cMinute);
+//   cSecond = addZero(cSecond);
+//   document.getElementById("currentTime").innerText = cHour + " : " + cMinute + " : " + cSecond + " " + AmPm;
+// }
+
+// function addZero(number) {
+//   if (number < 10) {
+//     return '0' + number;
+//   } else{
+//     return number;
+//   }
+// }
+
+
+// setInterval(displayTime, 1000);
+
+// displayTime();
+
+
+// function formatAMPM(currentDate) {
+//   let cHour = currentDate.getHours();
+//   let cMinute = currentDate.getMinutes();
+//   let cSecond = currentDate.getSeconds();
+//   let AmPm = cHour >= 12 ? "pm" : "am";
+//   cHour = cHour % 12;
+//   cHour = cHour ? cHour : 12; // the hour '0' should be '12'
+//   cMinutes = cMinutes < 10 ? "0" + minutes : minutes;
+//   var strTime = cHour + " : " + cMinute + " : " + cSecond + " " + AmPm;
+//   return strTime;
+// }
+
+// console.log(formatAMPM(new Date()));
+
+
+// function setAmPm() {
+// let AmPm = 'AM'
+// if (cHour > 12) {
+//   AmPm = 'PM'
+//   cHour = cHour - 12;
+//   }
+// }
+
+// setAmPm();
+
+
+
+// const AmPm = new Date().toLocaleTimeString("en-US", {
+//   hour: "numeric",
+//   minute: "numeric",
+//   seconds: "numeric",
+//   hour12: true,
+// });
+// console.log(AmPm);
+
+
+// let AmPm = cHour >= 12 ? "PM" : "AM";
+// cHour = cHour % 12
+
+// document.getElementById("currentDate").textContent = currentDate.toTimeString();
+
+
+// console.log(currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds());
 
 let daysOfTheWeek = [
   "Sunday",
@@ -28,8 +114,10 @@ let daysOfTheWeek = [
   "Saturday"
 ];
 
+
+
 // let = daysOfTheWeek[currentDate.getDay()];
-document.getElementById("currentDate").innerHTML = currentDate.toDateString();
+
 // console.log(daysOfTheWeek[currentDate.getDay()]);
 
 
@@ -49,9 +137,15 @@ let monthsOfTheYear = [
   "December"
 ];
 
+// To add a zero in front
 
-
-
+// function updateTime(k) {
+//   if (k < 10) {
+//     return "0" + k;
+//   } else {
+//     return k;
+//   }
+// }
 
 
 
